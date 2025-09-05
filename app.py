b7c5fb73e8cc1027ee60f4790dd49afd660b5f46
Vitor Santos <vg452004@gmail.com>
2025-09-05 15:07:25 -0300

    ADD db_agent

--- app.py
+++ app.py
@@ -1,33 +1,75 @@
+
+from dotenv import load_dotenv
+from langchain import hub
+from langchain.prompts import PromptTemplate
+from langchain.agents import create_react_agent, AgentExecutor
+from langchain_community.utilities.sql_database import SQLDatabase
+from langchain_community.agent_toolkits.sql.toolkit import SQLDatabaseToolkit
+from langchain_openai import ChatOpenAI
 import streamlit as st
 
+load_dotenv()
+
 st.set_page_config(
-    page_title='Stock Agent',
-    page_icon='/home/vitor/Documentos/PyCodeBR/ia_master/streamlit/stock_agent/assets/stock.png'
+    page_icon='assets/stock.png',
+    page_title='Stock Assistant'
 )
+st.header('I am a Stock Assistant')
 
-st.header('Stock Assistant')
-
-selection_options = [
+options = [
     'gpt-3.5-turbo',
     'gpt-4',
-    'gpt-4o-mini',
     'gpt-4o',
-    'gpt-4-turbo'
+    'gpt-4o-mini'
 ]
-model = st.sidebar.selectbox(
-    label='Model',
-    options = selection_options
+selected_model = st.sidebar.selectbox(
+    label='Select the LLM:', 
+    options=options
 )
-st.sidebar.markdown('### About')
-st.sidebar.markdown('This applicattion is a stock assistant!')
 
-st.write('Ask anything about the stock: ')
-user_question = st.text_input('Write your question...')
-send_button = st.button('Send')
+question = st.text_input('Ask a question about the stock:')
 
-if send_button:
-    if user_question:
-        st.write('Asked a question')
+model = ChatOpenAI(model= selected_model)
+
+db = SQLDatabase.from_uri('sqlite:///estoque.db')
+db_toolkit = SQLDatabaseToolkit(
+    db=db,
+    llm=model
+)
+
+system_message = hub.pull('hwchase17/react')
+
+agent = create_react_agent(
+    tools= db_toolkit.get_tools(),
+    llm=model,
+    prompt=system_message
+)
+db_agent = AgentExecutor(
+    tools=db_toolkit.get_tools(),
+    agent = agent,
+    handle_parsing_errors=True
+)
+
+prompt = '''
+Use as ferramentas necessárias para responder perguntas relacionadas ao
+estoque de produtos. Você fornecerá insights sobre produtos, preços, 
+reposição de estoque e relatórios conforme solicitado pelo usuário.
+A resposta final deve ter uma formatação amigável de visualização para o usuário.
+Sempre responda em português brasileiro.
+Pergunta: {q}
+'''
+prompt_template = PromptTemplate.from_template(prompt)
+
+if st.button('Send'):
+    if question:
+        with st.spinner('Generating the answer'):
+            response = db_agent.invoke(
+                {
+                    'input': prompt.format(q=question)
+                }
+            )
+
+            st.markdown(response.get('output'))
     
     else:
-        st.warning('Ask the agent a question!')
+        st.warning('You need to ask somethink to the agent!')
